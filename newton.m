b8ded728081e7352c085a71f076075ae00cf7a1c
Ritikace01 <59730128+Ritikace01@users.noreply.github.com>
2020-01-17 16:34:09 +0530

    Update newton.m

--- newton.m
+++ newton.m
@@ -2,7 +2,7 @@
 x0 = 0.5;
 x1 = x0 - (func(x0)/derivative(x0));
 j = 0;
-while(abs(x1 - x0)<0.00001)
+while(abs(x1 - x0)>0.00001)
     x0 = x1;
     x1 = x0 - (func(x0)/derivative(x0));
     j = j+1;
